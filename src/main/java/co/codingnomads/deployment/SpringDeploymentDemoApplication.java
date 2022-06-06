f333d1d8697bcefad6436a9b202884b85e9f7333
Jared Larsen <jaredjoel@me.com>
2022-06-06 14:36:51 -0500

    aws param test

--- src/main/java/co/codingnomads/deployment/SpringDeploymentDemoApplication.java
+++ src/main/java/co/codingnomads/deployment/SpringDeploymentDemoApplication.java
@@ -6,17 +6,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication(exclude = {
-		DataSourceAutoConfiguration.class,
-		DataSourceTransactionManagerAutoConfiguration.class,
-		HibernateJpaAutoConfiguration.class
-})
+@SpringBootApplication
 public class SpringDeploymentDemoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
