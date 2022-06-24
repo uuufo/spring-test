9ad5b0dac499ece194143f48decf12f41753261d
Jared Larsen <jaredjoel@me.com>
2022-06-24 14:55:14 -0700

    data out

--- src/main/java/co/codingnomads/deployment/SpringDeploymentDemoApplication.java
+++ src/main/java/co/codingnomads/deployment/SpringDeploymentDemoApplication.java
@@ -6,10 +6,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class SpringDeploymentDemoApplication {
 //	public class SpringDeploymentDemoApplication extends SpringBootServletInitializer {
 
